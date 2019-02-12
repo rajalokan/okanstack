7dcf04a443cbb188eb2f8403be6492a40f9387c4
Alok Kumar <rajalokan@gmail.com>
2019-02-12 07:59:40 +0530

    Refactored sclib

--- sclib.sh
+++ sclib.sh
@@ -45,49 +45,6 @@ function _source_file {
   fi
 }
 
-function get_pip {
-
-  # check if pip is already installed
-  if [ "$(which pip)" ]; then
-
-    # make sure that the right pip base packages are installed
-    # If this fails retry with --isolated to bypass the repo server because the repo server will not have
-    # been updated at this point to include any newer pip packages.
-    sudo -H pip install --upgrade ${PIP_INSTALL_OPTIONS} || sudo -H pip install --upgrade --isolated ${PIP_INSTALL_OPTIONS}
-
-    # Ensure that our shell knows about the new pip
-    hash -r pip
-
-  # when pip is not installed, install it
-  else
-
-    # If GET_PIP_URL is set, then just use it
-    if [ -n "${GET_PIP_URL:-}" ]; then
-      curl --silent ${GET_PIP_URL} > /opt/get-pip.py
-      if head -n 1 /opt/get-pip.py | grep python; then
-        python /opt/get-pip.py ${PIP_INSTALL_OPTIONS}
-        return
-      fi
-    fi
-
-    # Try getting pip from bootstrap.pypa.io as a primary source
-    curl --silent https://bootstrap.pypa.io/get-pip.py > /opt/get-pip.py
-    if head -n 1 /opt/get-pip.py | grep python; then
-      sudo -H python /opt/get-pip.py ${PIP_INSTALL_OPTIONS}
-      return
-    fi
-
-    # Try the get-pip.py from the github repository as a primary source
-    curl --silent https://raw.githubusercontent.com/pypa/get-pip/master/get-pip.py > /opt/get-pip.py
-    if head -n 1 /opt/get-pip.py | grep python; then
-      sudo -H python /opt/get-pip.py ${PIP_INSTALL_OPTIONS}
-      return
-    fi
-
-    echo "A suitable download location for get-pip.py could not be found."
-    exit_fail
-  fi
-}
 
 # ==============================================================================
 # GetOSVersion
@@ -185,104 +142,9 @@ function GetOSVersion {
     export os_VENDOR os_RELEASE os_UPDATE os_PACKAGE os_CODENAME
 }
 
-# Distro-agnostic function to tell if a package is installed
-# is_package_installed package [package ...]
-function is_package_installed {
-    if [[ -z "$@" ]]; then
-        return 1
-    fi
-
-    if [[ -z "$os_PACKAGE" ]]; then
-        GetOSVersion
-    fi
-
-    if [[ "$os_PACKAGE" = "deb" ]]; then
-        dpkg -s "$@" > /dev/null 2> /dev/null
-    elif [[ "$os_PACKAGE" = "rpm" ]]; then
-        rpm --quiet -q "$@"
-    else
-        exit_distro_not_supported "finding if a package is installed"
-    fi
-}
-
-# Determine if current distribution is a Fedora-based distribution
-# (Fedora, RHEL, CentOS, etc).
-# is_fedora
-function is_fedora {
-    if [[ -z "$os_VENDOR" ]]; then
-        GetOSVersion
-    fi
-
-    [ "$os_VENDOR" = "Fedora" ] || [ "$os_VENDOR" = "Red Hat" ] || \
-        [ "$os_VENDOR" = "CentOS" ] || [ "$os_VENDOR" = "OracleLinux" ] || \
-        [ "$os_VENDOR" = "CloudLinux" ]
-}
 
 
-# Determine if current distribution is a SUSE-based distribution
-# (openSUSE, SLE).
-# is_suse
-function is_suse {
-    if [[ -z "$os_VENDOR" ]]; then
-        GetOSVersion
-    fi
 
-    [ "$os_VENDOR" = "openSUSE" ] || [ "$os_VENDOR" = "SUSE LINUX" ]
-}
-
-# Determine if current distribution is an Ubuntu-based distribution
-# It will also detect non-Ubuntu but Debian-based distros
-# is_ubuntu
-function is_ubuntu {
-    if [[ -z "$os_PACKAGE" ]]; then
-        GetOSVersion
-    fi
-    [ "$os_PACKAGE" = "deb" ]
-}
-
-
-# Wrapper for ``yum`` to set proxy environment variables
-# Uses globals ``OFFLINE``, ``*_proxy``, ``YUM``
-# yum_install package [package ...]
-function yum_install {
-    [[ "$OFFLINE" = "True" ]] && return
-    local sudo="sudo"
-    [[ "$(id -u)" = "0" ]] && sudo="env"
-
-    # The manual check for missing packages is because yum -y assumes
-    # missing packages are OK.  See
-    # https://bugzilla.redhat.com/show_bug.cgi?id=965567
-    $sudo http_proxy="${http_proxy:-}" https_proxy="${https_proxy:-}" \
-        no_proxy="${no_proxy:-}" \
-        ${YUM:-yum} install -y "$@" 2>&1 | \
-        awk '
-            BEGIN { fail=0 }
-            /No package/ { fail=1 }
-            { print }
-            END { exit fail }' || \
-                die $LINENO "Missing packages detected"
-
-    # also ensure we catch a yum failure
-    if [[ ${PIPESTATUS[0]} != 0 ]]; then
-        die $LINENO "${YUM:-yum} install failure"
-    fi
-}
-
-# Wrapper for ``apt-get`` to set cache and proxy environment variables
-# Uses globals ``OFFLINE``, ``*_proxy``
-# apt_get operation package [package ...]
-function apt_get {
-    local xtrace=$(set +o | grep xtrace)
-    set +o xtrace
-    [[ "$OFFLINE" = "True" || -z "$@" ]] && return
-    local sudo="sudo"
-    [[ "$(id -u)" = "0" ]] && sudo="env"
-    $xtrace
-    $sudo DEBIAN_FRONTEND=noninteractive \
-        http_proxy=${http_proxy:-} https_proxy=${https_proxy:-} \
-        no_proxy=${no_proxy:-} \
-        apt-get --option "Dpkg::Options::=--force-confold" --assume-yes "$@"
-}
 
 # Distro-agnostic package installer
 # Uses globals ``NO_UPDATE_REPOS``, ``REPOS_UPDATED``, ``RETRY_UPDATE``
@@ -309,25 +171,7 @@ function update_package_repo {
     fi
 }
 
-function real_install_package {
-    if is_ubuntu; then
-        apt_get install "$@"
-    elif is_fedora; then
-        yum_install "$@"
-    elif is_suse; then
-        zypper_install "$@"
-    else
-        exit_distro_not_supported "installing packages"
-    fi
-}
 
-# Distro-agnostic package installer
-# install_package package [package ...]
-function install_package {
-    # update_package_repo
-    _log "Installing package $@"
-    real_install_package $@ || RETRY_UPDATE=True update_package_repo && real_install_package $@
-}
 
 function backup_if_present(){
     if [ -L $1 ]; then
@@ -503,4 +347,218 @@ function _preconfigure_instance {
         sudo yum install -y epel-release
         sudo yum install -y wget vim bash-completion
     fi
+}
+
+# ////////////////////////////////// Functions ////////////////////////////////
+
+# Distro-agnostic package installer
+# install_package package [package ...]
+function install_package {
+    # update_package_repo
+    _log "Installing package $@"
+    real_install_package $@ || RETRY_UPDATE=True update_package_repo && real_install_package $@
+}
+
+# get pip
+function get_pip {
+
+  # check if pip is already installed
+  if [ "$(which pip)" ]; then
+
+    # make sure that the right pip base packages are installed
+    # If this fails retry with --isolated to bypass the repo server because the repo server will not have
+    # been updated at this point to include any newer pip packages.
+    sudo -H pip install --upgrade ${PIP_INSTALL_OPTIONS} || sudo -H pip install --upgrade --isolated ${PIP_INSTALL_OPTIONS}
+
+    # Ensure that our shell knows about the new pip
+    hash -r pip
+
+  # when pip is not installed, install it
+  else
+
+    # If GET_PIP_URL is set, then just use it
+    if [ -n "${GET_PIP_URL:-}" ]; then
+      curl --silent ${GET_PIP_URL} > /opt/get-pip.py
+      if head -n 1 /opt/get-pip.py | grep python; then
+        python /opt/get-pip.py ${PIP_INSTALL_OPTIONS}
+        return
+      fi
+    fi
+
+    # Try getting pip from bootstrap.pypa.io as a primary source
+    curl --silent https://bootstrap.pypa.io/get-pip.py > /opt/get-pip.py
+    if head -n 1 /opt/get-pip.py | grep python; then
+      sudo -H python /opt/get-pip.py ${PIP_INSTALL_OPTIONS}
+      return
+    fi
+
+    # Try the get-pip.py from the github repository as a primary source
+    curl --silent https://raw.githubusercontent.com/pypa/get-pip/master/get-pip.py > /opt/get-pip.py
+    if head -n 1 /opt/get-pip.py | grep python; then
+      sudo -H python /opt/get-pip.py ${PIP_INSTALL_OPTIONS}
+      return
+    fi
+
+    echo "A suitable download location for get-pip.py could not be found."
+    exit_fail
+  fi
+}
+
+# Distro-agnostic function to tell if a package is installed
+# is_package_installed package [package ...]
+function is_package_installed {
+    if [[ -z "$@" ]]; then
+        return 1
+    fi
+
+    if [[ -z "$os_PACKAGE" ]]; then
+        GetOSVersion
+    fi
+
+    if [[ "$os_PACKAGE" = "deb" ]]; then
+        dpkg -s "$@" > /dev/null 2> /dev/null
+    elif [[ "$os_PACKAGE" = "rpm" ]]; then
+        rpm --quiet -q "$@"
+    else
+        exit_distro_not_supported "finding if a package is installed"
+    fi
+}
+
+# //////////////////////////////// Private Functions //////////////////////////
+
+# ///////// Install Package
+
+# Real function for installing a package
+function real_install_package {
+    if is_ubuntu; then
+        apt_get install "$@"
+    elif is_fedora; then
+        yum_install "$@"
+    else
+        exit_distro_not_supported "installing packages"
+    fi
+}
+
+# Wrapper for ``apt-get`` to set cache and proxy environment variables
+# Uses globals ``OFFLINE``, ``*_proxy``
+# apt_get operation package [package ...]
+function apt_get {
+    local xtrace=$(set +o | grep xtrace)
+    set +o xtrace
+    [[ "$OFFLINE" = "True" || -z "$@" ]] && return
+    local sudo="sudo"
+    [[ "$(id -u)" = "0" ]] && sudo="env"
+    $xtrace
+    $sudo DEBIAN_FRONTEND=noninteractive \
+        http_proxy=${http_proxy:-} https_proxy=${https_proxy:-} \
+        no_proxy=${no_proxy:-} \
+        apt-get --option "Dpkg::Options::=--force-confold" --assume-yes "$@"
+}
+
+# Wrapper for ``yum`` to set proxy environment variables
+# Uses globals ``OFFLINE``, ``*_proxy``, ``YUM``
+# yum_install package [package ...]
+function yum_install {
+    [[ "$OFFLINE" = "True" ]] && return
+    local sudo="sudo"
+    [[ "$(id -u)" = "0" ]] && sudo="env"
+
+    # The manual check for missing packages is because yum -y assumes
+    # missing packages are OK.  See
+    # https://bugzilla.redhat.com/show_bug.cgi?id=965567
+    $sudo http_proxy="${http_proxy:-}" https_proxy="${https_proxy:-}" \
+        no_proxy="${no_proxy:-}" \
+        ${YUM:-yum} install -y "$@" 2>&1 | \
+        awk '
+            BEGIN { fail=0 }
+            /No package/ { fail=1 }
+            { print }
+            END { exit fail }' || \
+                die $LINENO "Missing packages detected"
+
+    # also ensure we catch a yum failure
+    if [[ ${PIPESTATUS[0]} != 0 ]]; then
+        die $LINENO "${YUM:-yum} install failure"
+    fi
+}
+
+# ///////// OS Type
+
+# Determine if current distribution is a Fedora-based distribution
+# (Fedora, RHEL, CentOS, etc).
+# is_fedora
+function is_fedora {
+    if [[ -z "$os_VENDOR" ]]; then
+        GetOSVersion
+    fi
+
+    [ "$os_VENDOR" = "Fedora" ] || [ "$os_VENDOR" = "Red Hat" ] || \
+        [ "$os_VENDOR" = "CentOS" ] || [ "$os_VENDOR" = "OracleLinux" ] || \
+        [ "$os_VENDOR" = "CloudLinux" ]
+}
+
+
+# Determine if current distribution is a SUSE-based distribution
+# (openSUSE, SLE).
+# is_suse
+function is_suse {
+    if [[ -z "$os_VENDOR" ]]; then
+        GetOSVersion
+    fi
+
+    [ "$os_VENDOR" = "openSUSE" ] || [ "$os_VENDOR" = "SUSE LINUX" ]
+}
+
+# Determine if current distribution is an Ubuntu-based distribution
+# It will also detect non-Ubuntu but Debian-based distros
+# is_ubuntu
+function is_ubuntu {
+    if [[ -z "$os_PACKAGE" ]]; then
+        GetOSVersion
+    fi
+    [ "$os_PACKAGE" = "deb" ]
+}
+
+# //////////////////////////////////////////////////////////////////////////////
+function setup_bash(parameter) {
+    base_url="https://raw.githubusercontent.com/rajalokan/ansible-role-dotfiles/master/files/bash"
+    if is_ubuntu; then
+        # Fetch bashrc & profile
+        file_path="$base_url/ubuntu/$os_RELEASE"
+        wget -q "$file_path/bashrc" -O ~/.bashrc
+        wget -q "$file_path/profile" -O ~/.profile
+    elif is_fedora; then
+        # Fetch bashrc & bash_profile
+        file_path="$base_url/centos"
+        wget -q "$file_path/bashrc" -O ~/.bashrc
+        wget -q "$file_path/bash_profile" -O ~/.bash_profile
+    else
+        exit_distro_not_supported "installing packages"
+    fi
+    # copy bashrc_okan
+    wget -q "$base_url/bashrc_okan" -O ~/.bashrc_okan
+    # copy bash_aliases
+    wget -q "$base_url/bash_aliases" -O ~/.bash_aliases
+
+    # Pureline
+    pureline_url="https://raw.githubusercontent.com/chris-marsh/pureline/master/pureline"
+    wget -q $pureline_url -O /tmp/pureline
+    wget -q "$base_url/pureline.conf" -O ~/.pureline.conf
+}
+
+function setup_vim(parameter) {
+    is_package_installed vim || install_package vim
+}
+
+function setup_git(parameter) {
+    is_package_installed git || install_package git
+    base_url="https://raw.githubusercontent.com/rajalokan/ansible-role-dotfiles/master/files/git"
+    wget -q "$file_path/gitconfig" -O ~/.gitconfig
+    wget -q "$file_path/gitignore" -O ~/.gitignore
+}
+
+function setup_tmux(parameter) {
+    is_package_installed tmux || install_package tmux
+    base_url="https://raw.githubusercontent.com/rajalokan/ansible-role-dotfiles/master/files/tmux"
+    wget -q "$file_path/tmux.conf" -O ~/.tmux.conf
 }
