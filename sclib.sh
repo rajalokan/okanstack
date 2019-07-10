1250f3d2f528a12a4e6ae9771e7ff5edad024e65
Alok Kumar <rajalokan@gmail.com>
2019-07-10 10:44:18 +0530

    refactored

--- sclib.sh
+++ sclib.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
-## Vars ----------------------------------------------------------------------
-# PIP_INSTALL_OPTIONS=${PIP_INSTALL_OPTIONS:-'pip==9.0.1 setuptools==33.1.1 wheel==0.29.0 '}
+# /////////////////////////  Variables ////////////////////////////////////////
+
 PIP_INSTALL_OPTIONS=${PIP_INSTALL_OPTIONS:-'pip==9.0.1'}
+base_url="https://raw.githubusercontent.com/rajalokan/ansible-role-dotfiles/master/files/bash"
+pureline_url="https://raw.githubusercontent.com/chris-marsh/pureline/master/pureline"
+
+# //////////////////////////  Sclib Functions /////////////////////////////////
 
 function is_sclib_imported {
     [[ 0 ]]
@@ -11,9 +15,7 @@ function is_sclib_imported {
 function print_info {
   RED='\033[0;33m'
   NC='\033[0m' # No Color
-  # PROC_NAME="- [ $@ ] -"
   PROC_NAME="${RED}- [ $@ ] -${NC}"
-  # printf "\n%s%s\n" "$PROC_NAME" "${LINE:${#PROC_NAME}}"
   printf "$PROC_NAME\n"
 }
 
@@ -35,9 +37,6 @@ function _error {
   printf "${RED}$@${NC}\n"
 }
 
-function GetanotherVersion {
- echo "Not Implemented"
-}
 
 function _source_file {
   if [[ ! -z $1 ]]; then
@@ -45,9 +44,8 @@ function _source_file {
   fi
 }
 
+# ////////////////// Operating System related Functions ///////////////////////
 
-# ==============================================================================
-# GetOSVersion
 function GetOSVersion {
 
     # Figure out which vendor we are
@@ -142,9 +140,9 @@ function GetOSVersion {
     export os_VENDOR os_RELEASE os_UPDATE os_PACKAGE os_CODENAME
 }
 
-
-
-
+function GetanotherVersion {
+    echo "Not Implemented"
+}
 
 # Distro-agnostic package installer
 # Uses globals ``NO_UPDATE_REPOS``, ``REPOS_UPDATED``, ``RETRY_UPDATE``
@@ -171,65 +169,192 @@ function update_package_repo {
     fi
 }
 
+# Distro-agnostic package installer
+# install_package package [package ...]
+function install_package {
+    # update_package_repo
+    _log "Installing package $@"
+    real_install_package $@ || RETRY_UPDATE=True update_package_repo && real_install_package $@
+}
 
+# Distro-agnostic function to tell if a package is installed
+# is_package_installed package [package ...]
+function is_package_installed {
+    if [[ -z "$@" ]]; then
+        return 1
+    fi
 
-function backup_if_present(){
-    if [ -L $1 ]; then
-        _log "Deleting link $1"
-        rm $1
-    elif [[ -f $1 ]] || [[ -d $2 ]]; then
-        _log "Backing up $1 to $1.bak.$(date +"%d_%m_%Y_%H%M%S")"
-        mv $1 $1.bak.$(date +"%d_%m_%Y_%H%M%S")
+    if [[ -z "$os_PACKAGE" ]]; then
+        GetOSVersion
+    fi
+
+    if [[ "$os_PACKAGE" = "deb" ]]; then
+        dpkg -s "$@" > /dev/null 2> /dev/null
+    elif [[ "$os_PACKAGE" = "rpm" ]]; then
+        rpm --quiet -q "$@"
+    else
+        exit_distro_not_supported "finding if a package is installed"
     fi
 }
 
-function run_ansible_role {
-    role=$1
-    [[ -z $3 ]] && TAGS="" || TAGS="--tags $3"
-    _log "Running ansible role $1"
+# Real function for installing a package
+function real_install_package {
+    if is_ubuntu; then
+        apt_get install "$@"
+    elif is_fedora; then
+        yum_install "$@"
+    else
+        exit_distro_not_supported "installing packages"
+    fi
+}
 
-    ansible_roles_path="${HOME}/.ansible/roles"
-    mkdir -p ${ansible_roles_path}
+# Wrapper for ``apt-get`` to set cache and proxy environment variables
+# Uses globals ``OFFLINE``, ``*_proxy``
+# apt_get operation package [package ...]
+function apt_get {
+    local xtrace=$(set +o | grep xtrace)
+    set +o xtrace
+    [[ "$OFFLINE" = "True" || -z "$@" ]] && return
+    local sudo="sudo"
+    [[ "$(id -u)" = "0" ]] && sudo="env"
+    $xtrace
+    $sudo DEBIAN_FRONTEND=noninteractive \
+        http_proxy=${http_proxy:-} https_proxy=${https_proxy:-} \
+        no_proxy=${no_proxy:-} \
+        apt-get --option "Dpkg::Options::=--force-confold" --assume-yes "$@"
+}
 
-    role_path="${ansible_roles_path}/$1"
-    role_repo="https://github.com/rajalokan/ansible-role-$1"
+# Wrapper for ``yum`` to set proxy environment variables
+# Uses globals ``OFFLINE``, ``*_proxy``, ``YUM``
+# yum_install package [package ...]
+function yum_install {
+    [[ "$OFFLINE" = "True" ]] && return
+    local sudo="sudo"
+    [[ "$(id -u)" = "0" ]] && sudo="env"
 
-    # Ensure git is installed
+    # The manual check for missing packages is because yum -y assumes
+    # missing packages are OK.  See
+    # https://bugzilla.redhat.com/show_bug.cgi?id=965567
+    $sudo http_proxy="${http_proxy:-}" https_proxy="${https_proxy:-}" \
+        no_proxy="${no_proxy:-}" \
+        ${YUM:-yum} install -y "$@" 2>&1 | \
+        awk '
+            BEGIN { fail=0 }
+            /No package/ { fail=1 }
+            { print }
+            END { exit fail }' || \
+                die $LINENO "Missing packages detected"
+
+    # also ensure we catch a yum failure
+    if [[ ${PIPESTATUS[0]} != 0 ]]; then
+        die $LINENO "${YUM:-yum} install failure"
+    fi
+}
+
+# Determine if current distribution is a Fedora-based distribution
+# (Fedora, RHEL, CentOS, etc).
+# is_fedora
+function is_fedora {
+    if [[ -z "$os_VENDOR" ]]; then
+        GetOSVersion
+    fi
+
+    [ "$os_VENDOR" = "Fedora" ] || [ "$os_VENDOR" = "Red Hat" ] || \
+        [ "$os_VENDOR" = "CentOS" ] || [ "$os_VENDOR" = "OracleLinux" ] || \
+        [ "$os_VENDOR" = "CloudLinux" ]
+}
+
+# Determine if current distribution is a SUSE-based distribution
+# (openSUSE, SLE).
+# is_suse
+function is_suse {
+    if [[ -z "$os_VENDOR" ]]; then
+        GetOSVersion
+    fi
+
+    [ "$os_VENDOR" = "openSUSE" ] || [ "$os_VENDOR" = "SUSE LINUX" ]
+}
+
+# Determine if current distribution is an Ubuntu-based distribution
+# It will also detect non-Ubuntu but Debian-based distros
+# is_ubuntu
+function is_ubuntu {
+    if [[ -z "$os_PACKAGE" ]]; then
+        GetOSVersion
+    fi
+    [ "$os_PACKAGE" = "deb" ]
+}
+
+# ////////////////////////// Setup VM Functions ///////////////////////////////
+
+function setup_all {
+    GetOSVersion
+    SERVER_NAME=${1:-}
+    sudo hostname ${SERVER_NAME}
+    grep -q ${SERVER_NAME} /etc/hosts || sudo sed -i "2i127.0.1.1  ${SERVER_NAME}" /etc/hosts
+
+    if [[ ${os_VENDOR,,} == "ubuntu" ]]; then
+        sudo apt update
+    elif [[ ${os_VENDOR,,} == "centos" ]]; then
+        sudo yum install -y epel-release
+        sudo yum install -y wget vim bash-completion
+    fi
+    setup_bash
+    setup_vim
+    setup_git
+}
+
+function setup_bash() {
+    GetOSVersion
+    base_url="https://raw.githubusercontent.com/rajalokan/ansible-role-dotfiles/master/files/bash"
+    if is_ubuntu; then
+        # Fetch bashrc & profile
+        file_path="$base_url/ubuntu/$os_RELEASE"
+        wget -q "$file_path/bashrc" -O ~/.bashrc
+        wget -q "$file_path/profile" -O ~/.profile
+    elif is_fedora; then
+        # Fetch bashrc & bash_profile
+        file_path="$base_url/centos"
+        wget -q "$file_path/bashrc" -O ~/.bashrc
+        wget -q "$file_path/bash_profile" -O ~/.bash_profile
+    else
+        exit_distro_not_supported "installing packages"
+    fi
+    # copy bashrc_okan
+    wget -q "$base_url/bashrc_okan" -O ~/.bashrc_okan
+    # copy bash_aliases
+    wget -q "$base_url/bash_aliases" -O ~/.bash_aliases
+
+    # Pureline
+    pureline_url="https://raw.githubusercontent.com/chris-marsh/pureline/master/pureline"
+    wget -q $pureline_url -O /tmp/pureline
+    wget -q "$base_url/pureline.conf" -O ~/.pureline.conf
+}
+
+function setup_vim() {
+    is_package_installed vim || install_package vim
+}
+
+function setup_git() {
     is_package_installed git || install_package git
-
-    if [[ ! -d ${role_path} ]]; then
-        git clone ${role_repo} ${role_path}
-    fi
-
-    # Ensure latest ansible is installed
-    is_package_installed ansible || _install_ansible
-
-    pushd ${role_path} >/dev/null
-        ansible-playbook -i "localhost," -c local ${TAGS} playbook.yaml
-    popd
+    base_url="https://raw.githubusercontent.com/rajalokan/ansible-role-dotfiles/master/files/git"
+    wget -q "$base_url/gitconfig" -O ~/.gitconfig
+    wget -q "$base_url/gitignore" -O ~/.gitignore
 }
 
-function _install_ansible {
-    if ! is_package_installed ansible; then
-        if is_ubuntu; then
-            sudo apt-add-repository -y ppa:ansible/ansible
-            sudo apt update
-        else
-            sudo yum install -y epel-release
-        fi
-        install_package ansible
-    fi
+function setup_tmux() {
+    is_package_installed tmux || install_package tmux
+    base_url="https://raw.githubusercontent.com/rajalokan/ansible-role-dotfiles/master/files/tmux"
+    wget -q "$file_path/tmux.conf" -O ~/.tmux.conf
 }
 
-# ==============================================================================
-# Bootstrapping a new VM
-# ==============================================================================
+# /////////////////////// OpenStack Functions /////////////////////////////////
 
-function boot_vm {
+function bootstrap_openstack_vm {
     _display_inputs
-    # _boot
-    # _add_floating_ip
-    # _assign_instance_ip
+    _boot
+    _add_floating_ip
+    _assign_instance_ip
     _add_to_ssh_config
     _verify_ssh
     _preconfigure_instance ${SERVER_NAME}
@@ -346,36 +471,59 @@ function _preconfigure_instance {
         [[ -f /tmp/sclib.sh ]] || wget -q  $url -O /tmp/sclib.sh
         source /tmp/sclib.sh
 
-        _preconfigure_vm
+        setup_all
 EOF
     _log "Successfully preconfigured ${SERVER_NAME}"
 }
 
-function _preconfigure_vm {
-    GetOSVersion
-    SERVER_NAME=${1:-}
-    sudo hostname ${SERVER_NAME}
-    grep -q ${SERVER_NAME} /etc/hosts || sudo sed -i "2i127.0.1.1  ${SERVER_NAME}" /etc/hosts
+# //////////////////////////// Helper Functions ///////////////////////////////
 
-    if [[ ${os_VENDOR,,} == "ubuntu" ]]; then
-        sudo apt update
-    elif [[ ${os_VENDOR,,} == "centos" ]]; then
-        sudo yum install -y epel-release
-        sudo yum install -y wget vim bash-completion
+function backup_if_present(){
+    if [ -L $1 ]; then
+        _log "Deleting link $1"
+        rm $1
+    elif [[ -f $1 ]] || [[ -d $2 ]]; then
+        _log "Backing up $1 to $1.bak.$(date +"%d_%m_%Y_%H%M%S")"
+        mv $1 $1.bak.$(date +"%d_%m_%Y_%H%M%S")
     fi
-    setup_bash
-    setup_vim
-    setup_git
 }
 
-# ////////////////////////////////// Functions ////////////////////////////////
+function run_ansible_role {
+    role=$1
+    [[ -z $3 ]] && TAGS="" || TAGS="--tags $3"
+    _log "Running ansible role $1"
 
-# Distro-agnostic package installer
-# install_package package [package ...]
-function install_package {
-    # update_package_repo
-    _log "Installing package $@"
-    real_install_package $@ || RETRY_UPDATE=True update_package_repo && real_install_package $@
+    ansible_roles_path="${HOME}/.ansible/roles"
+    mkdir -p ${ansible_roles_path}
+
+    role_path="${ansible_roles_path}/$1"
+    role_repo="https://github.com/rajalokan/ansible-role-$1"
+
+    # Ensure git is installed
+    is_package_installed git || install_package git
+
+    if [[ ! -d ${role_path} ]]; then
+        git clone ${role_repo} ${role_path}
+    fi
+
+    # Ensure latest ansible is installed
+    is_package_installed ansible || _install_ansible
+
+    pushd ${role_path} >/dev/null
+        ansible-playbook -i "localhost," -c local ${TAGS} playbook.yaml
+    popd
+}
+
+function _install_ansible {
+    if ! is_package_installed ansible; then
+        if is_ubuntu; then
+            sudo apt-add-repository -y ppa:ansible/ansible
+            sudo apt update
+        else
+            sudo yum install -y epel-release
+        fi
+        install_package ansible
+    fi
 }
 
 # get pip
@@ -423,230 +571,4 @@ function get_pip {
   fi
 }
 
-# Distro-agnostic function to tell if a package is installed
-# is_package_installed package [package ...]
-function is_package_installed {
-    if [[ -z "$@" ]]; then
-        return 1
-    fi
-
-    if [[ -z "$os_PACKAGE" ]]; then
-        GetOSVersion
-    fi
-
-    if [[ "$os_PACKAGE" = "deb" ]]; then
-        dpkg -s "$@" > /dev/null 2> /dev/null
-    elif [[ "$os_PACKAGE" = "rpm" ]]; then
-        rpm --quiet -q "$@"
-    else
-        exit_distro_not_supported "finding if a package is installed"
-    fi
-}
-
-# //////////////////////////////// Private Functions //////////////////////////
-
-# ///////// Install Package
-
-# Real function for installing a package
-function real_install_package {
-    if is_ubuntu; then
-        apt_get install "$@"
-    elif is_fedora; then
-        yum_install "$@"
-    else
-        exit_distro_not_supported "installing packages"
-    fi
-}
-
-# Wrapper for ``apt-get`` to set cache and proxy environment variables
-# Uses globals ``OFFLINE``, ``*_proxy``
-# apt_get operation package [package ...]
-function apt_get {
-    local xtrace=$(set +o | grep xtrace)
-    set +o xtrace
-    [[ "$OFFLINE" = "True" || -z "$@" ]] && return
-    local sudo="sudo"
-    [[ "$(id -u)" = "0" ]] && sudo="env"
-    $xtrace
-    $sudo DEBIAN_FRONTEND=noninteractive \
-        http_proxy=${http_proxy:-} https_proxy=${https_proxy:-} \
-        no_proxy=${no_proxy:-} \
-        apt-get --option "Dpkg::Options::=--force-confold" --assume-yes "$@"
-}
-
-# Wrapper for ``yum`` to set proxy environment variables
-# Uses globals ``OFFLINE``, ``*_proxy``, ``YUM``
-# yum_install package [package ...]
-function yum_install {
-    [[ "$OFFLINE" = "True" ]] && return
-    local sudo="sudo"
-    [[ "$(id -u)" = "0" ]] && sudo="env"
-
-    # The manual check for missing packages is because yum -y assumes
-    # missing packages are OK.  See
-    # https://bugzilla.redhat.com/show_bug.cgi?id=965567
-    $sudo http_proxy="${http_proxy:-}" https_proxy="${https_proxy:-}" \
-        no_proxy="${no_proxy:-}" \
-        ${YUM:-yum} install -y "$@" 2>&1 | \
-        awk '
-            BEGIN { fail=0 }
-            /No package/ { fail=1 }
-            { print }
-            END { exit fail }' || \
-                die $LINENO "Missing packages detected"
-
-    # also ensure we catch a yum failure
-    if [[ ${PIPESTATUS[0]} != 0 ]]; then
-        die $LINENO "${YUM:-yum} install failure"
-    fi
-}
-
-# ///////// OS Type
-
-# Determine if current distribution is a Fedora-based distribution
-# (Fedora, RHEL, CentOS, etc).
-# is_fedora
-function is_fedora {
-    if [[ -z "$os_VENDOR" ]]; then
-        GetOSVersion
-    fi
-
-    [ "$os_VENDOR" = "Fedora" ] || [ "$os_VENDOR" = "Red Hat" ] || \
-        [ "$os_VENDOR" = "CentOS" ] || [ "$os_VENDOR" = "OracleLinux" ] || \
-        [ "$os_VENDOR" = "CloudLinux" ]
-}
-
-
-# Determine if current distribution is a SUSE-based distribution
-# (openSUSE, SLE).
-# is_suse
-function is_suse {
-    if [[ -z "$os_VENDOR" ]]; then
-        GetOSVersion
-    fi
-
-    [ "$os_VENDOR" = "openSUSE" ] || [ "$os_VENDOR" = "SUSE LINUX" ]
-}
-
-# Determine if current distribution is an Ubuntu-based distribution
-# It will also detect non-Ubuntu but Debian-based distros
-# is_ubuntu
-function is_ubuntu {
-    if [[ -z "$os_PACKAGE" ]]; then
-        GetOSVersion
-    fi
-    [ "$os_PACKAGE" = "deb" ]
-}
-
-# //////////////////////////////////////////////////////////////////////////////
-function setup_bash() {
-    GetOSVersion
-    base_url="https://raw.githubusercontent.com/rajalokan/ansible-role-dotfiles/master/files/bash"
-    if is_ubuntu; then
-        # Fetch bashrc & profile
-        file_path="$base_url/ubuntu/$os_RELEASE"
-        wget -q "$file_path/bashrc" -O ~/.bashrc
-        wget -q "$file_path/profile" -O ~/.profile
-    elif is_fedora; then
-        # Fetch bashrc & bash_profile
-        file_path="$base_url/centos"
-        wget -q "$file_path/bashrc" -O ~/.bashrc
-        wget -q "$file_path/bash_profile" -O ~/.bash_profile
-    else
-        exit_distro_not_supported "installing packages"
-    fi
-    # copy bashrc_okan
-    wget -q "$base_url/bashrc_okan" -O ~/.bashrc_okan
-    # copy bash_aliases
-    wget -q "$base_url/bash_aliases" -O ~/.bash_aliases
-
-    # Pureline
-    pureline_url="https://raw.githubusercontent.com/chris-marsh/pureline/master/pureline"
-    wget -q $pureline_url -O /tmp/pureline
-    wget -q "$base_url/pureline.conf" -O ~/.pureline.conf
-}
-
-function setup_vim() {
-    is_package_installed vim || install_package vim
-}
-
-function setup_git() {
-    is_package_installed git || install_package git
-    base_url="https://raw.githubusercontent.com/rajalokan/ansible-role-dotfiles/master/files/git"
-    wget -q "$base_url/gitconfig" -O ~/.gitconfig
-    wget -q "$base_url/gitignore" -O ~/.gitignore
-}
-
-function setup_tmux() {
-    is_package_installed tmux || install_package tmux
-    base_url="https://raw.githubusercontent.com/rajalokan/ansible-role-dotfiles/master/files/tmux"
-    wget -q "$file_path/tmux.conf" -O ~/.tmux.conf
-}
-
-
-function bootstrap_vm() {
-    SERVER_NAME=${1:-"playbox"}
-    INSTANCE_NAME=${SERVER_NAME}-alok
-    OS_TYPE=${2:-"centos"}
-
-    source /tmp/secrets
-    _log "Booting VM ${INSTANCE_NAME}"
-
-    # Create server instance
-    openstack server create \
-        --image ${IMAGE} \
-        --flavor ${FLAVOR} \
-        --security-group ${SEC_GROUP} \
-        --key-name ${KEY_NAME} \
-        --network ${NETWORK} \
-        ${INSTANCE_NAME}
-
-    for i in {1..5}; do
-        if [[ $(openstack server show ${INSTANCE_NAME} -f value -c status) == "ACTIVE" ]]; then
-            break
-        else
-            _log "Waiting for server to become ACTIVE"
-            sleep 5;
-        fi
-    done
-
-    # Attach floating ip
-    PUBLIC_IP=$(openstack floating ip list --status DOWN -f value -c "Floating IP Address" | head -n 1)
-    _log "Attaching floating IP ${PUBLIC_IP}"
-    openstack server add floating ip --insecure ${INSTANCE_NAME} ${PUBLIC_IP}
-
-    # Add to ssh config
-    _log "Adding to ssh config file"
-    host_string="Host ${SERVER_NAME}"
-
-    read -d '' new_host_string <<- EOM
-Host ${SERVER_NAME}
-  HostName ${PUBLIC_IP}
-  User ${SSH_USER}
-  IdentityFile ~/.ssh/${KEY_NAME}.pem
-EOM
-
-    search=`grep "${host_string}" ~/.ssh/config`
-    if [[ ${search} = ${host_string} ]]; then
-        sed -i "/${host_string}/,+3 d" ~/.ssh/config
-        echo "$new_host_string" >> ~/.ssh/config
-    else
-        echo "$new_host_string" >> ~/.ssh/config
-    fi
-
-    sleep 5
-    # Verify SSH
-    ssh -T ${SERVER_NAME} exit && [ $? == 0 ] && _log "Success" \
-        || {
-            _error "Error. Trying again"; \
-            ssh-keygen -f "/home/${USER}/.ssh/known_hosts" -R ${PUBLIC_IP}; \
-            ssh ${SERVER_NAME} exit && _log "Success after adding correct host key." || _error "Failed"; \
-        }
-
-    # Configure instance
-    _log "Configuring ${INSTANCE_NAME}"
-    ssh -T ${SERVER_NAME} << EOF
-sudo hostname ${SERVER_NAME}
-grep -q ${SERVER_NAME} /etc/hosts || sudo sed -i "2i127.0.1.1   ${SERVER_NAME}" /etc/hosts
-EOF
-}
+# //////////////////////////////// END  ///////////////////////////////////////
